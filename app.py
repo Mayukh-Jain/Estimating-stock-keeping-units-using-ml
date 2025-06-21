614ccdff0876a22eca7c1504f0f77855f080b198
Mayukh-Jain <jainmayukh@gmail.com>
2025-06-21 14:34:30 +0530

    Updated flask

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pickle
 from flask import Flask, request, render_template
+
+
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# Create Flask app
 app = Flask(__name__)
 
-# Load trained model
 model = pickle.load(open("sales_demand_forecasting.pkl", "rb"))
 
 @app.route('/')
@@ -17,12 +17,15 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        input_features = [float(x) for x in request.form.values()]
+        total_price = float(request.form['total_price'])
+        base_price = float(request.form['base_price'])
+        is_featured = float(request.form['is_featured_sku'])
+        is_display = float(request.form['is_display_sku'])
 
-        final_features = [np.array(input_features)]
-        prediction = model.predict(final_features)
-        output = 'Fraudulent Claim' if prediction[0] == 1 else 'Genuine Claim'
-        return render_template('result.html', prediction_text=output)
+        input_data = np.array([[total_price, base_price, is_featured, is_display]])
+        prediction = model.predict(input_data)
+
+        return render_template('result.html', prediction_text=f"Forecasted Demand: {round(prediction[0], 2)} units")
     except Exception as e:
         return render_template('result.html', prediction_text=f"Error: {str(e)}")
 
